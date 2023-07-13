cf3f8c5d4c68e7bbd8a2f4c6354498948d924888
“HANANEZOUBIgit config --global user.email “hananezoubi1@gmail.com <hananezoubi1@gmail.com>
2023-07-13 13:23:39 -0700

    6-size.c

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+
+/**
+ * main - Prints the size of various types based on
+ *the coputer it is compiled and run on..
+ * Return: Always 0 (Success)
+ */
+
+
+int main(void)
+
+
+{
+	printf("Size of a char: %zu byte(s),\n",Sizeof(char));
+	printf("Size of a int: %zu byte(s),\n",Sizeof(int));
+	printf("Size of a long: %zu byte(s),\n",Sizeof(long));
+	printf("Size of a long int: %zu byte(s),\n",Sizeof(long int));
+	printf("Size of a long long int: %zu byte(s),\n",Sizeof(long long int));
+	printf("Size of a float: %zu byte(s),\n",Sizeof(float));
+	return (0);
+}
