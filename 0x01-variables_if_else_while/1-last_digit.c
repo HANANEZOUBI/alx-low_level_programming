7e685f498cb0706aaed52e04751fc120e3d18759
“HANANEZOUBIgit config --global user.email “hananezoubi1@gmail.com <hananezoubi1@gmail.com>
2023-07-15 03:26:42 -0700

    1-last_digit

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -1,4 +1,6 @@
-#include <time.h>
+#include <stdlib.h>
+
+#include<time.h>
 
 #include <stdio.h>
 
@@ -15,8 +17,8 @@ int main(void)
 	int n, lastd;
 
 	srand(time(0));
-	n = rand() - RAND MAX / 2;
-	lastd= n % 10;
+	n = rand() - RAND_MAX / 2;
+	lastd = n % 10;
 
 	if (lastd > 5)
 	{
@@ -29,6 +31,6 @@ int main(void)
 	else if (lastd < 6 && lastd != 0)
 	{
 	printf("Last digit of %d is %d and is less than 6 and not 0\n", n, lastd);
-	}	
+	}
 	return (0);
-}	
+}
