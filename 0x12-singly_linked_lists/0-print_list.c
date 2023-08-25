ad0b980675c03df9f67c15004be3669ce69f3e0b
HANANEZOUBI <hananezoubi1@gmail.com>
2023-08-25 14:54:53 -0700

    print_lists

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -16,13 +16,13 @@ size_t print_list(const list_t *h)
 
 	while (h != NULL)
 	{
-		c++;
 		if (h->str == NULL)
 			printf("[0] (nil)\n");
 		else
 			printf("[%d] %s\n", h->len, h->str);
 
 		h = h->next;
+		c++;
 	}
 
 	return (c);
