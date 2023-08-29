73579b70968feda571ad71c1cc33e9de943ee752
HANANEZOUBI <hananezoubi1@gmail.com>
2023-08-29 05:00:37 -0700

    h

---
+++ 0x13-more_singly_linked_lists/1-listint_len.c
@@ -0,0 +1,20 @@
+#include "lists.h"
+
+/**
+ * print_listint - print all the elements of a linked list.
+ * @h: head of a list.
+ *
+ * Return: numbers of nodes.
+ */
+
+size_t print_listint(const listint_t *h)
+{
+        size_t c = 0;
+
+        while (h != NULL)
+        {
+                h = h->next;
+                c++;
+        }
+        return (c);
+}
