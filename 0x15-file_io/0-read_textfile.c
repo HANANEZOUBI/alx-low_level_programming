c77b4cc2b01157911bf85ab24a82d1dd08ff5096
HANANEZOUBI <hananezoubi1@gmail.com>
2023-09-04 15:16:52 -0700

    h

---
+++ 0x15-file_io/0-read_textfile.c
@@ -0,0 +1,39 @@
+#include "main.h"
+
+/**
+ * read_textfile - to  read a text file and print it
+ *
+ * @filename: the name of file to read
+ *
+ * @letters: number of letters to read
+ *
+ * Return: number of letters read or 0
+ **/
+
+ssize_t read_textfile(const char *filename, size_t letters)
+{
+	int doc;
+	ssize_t nr, nw;
+	char *buf;
+
+	if (!filename)
+		return (0);
+
+	doc = open(filename, O_RDONLY);
+
+	if (fd == -1)
+		return (0);
+
+	buf = malloc(sizeof(char) * (letters));
+	if (!buf)
+		return (0);
+
+	nr = read(doc, buf, letters);
+	nw = write(STDOUT_FILENO, buf, nr);
+
+	close(doc);
+
+	free(buf);
+
+	return (nw);
+}
