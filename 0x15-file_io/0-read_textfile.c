33149e5fcb39b6b9dd3d2cda7c9c552861fcb457
HANANEZOUBI <hananezoubi1@gmail.com>
2023-09-04 15:26:02 -0700

    h

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -21,7 +21,7 @@ ssize_t read_textfile(const char *filename, size_t letters)
 
 	doc = open(filename, O_RDONLY);
 
-	if (fd == -1)
+	if (doc == -1)
 		return (0);
 
 	buf = malloc(sizeof(char) * (letters));
