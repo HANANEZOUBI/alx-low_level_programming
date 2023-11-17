451df2a04740ae7db0bc240b5250ac05b0b09ac5
HANANEZOUBI <hananezoubi1@gmail.com>
2023-11-17 14:03:38 -0800

    h

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -6,6 +6,7 @@
  * @name : dog's name
  * @age : dog's age
  * @owner : dog's owner
+ *
  * DESCRIPTION : the structure of a dog
  **/
 
@@ -20,4 +21,5 @@ void init_dog(struct dog *d, char *name, float age, char *owner);
 void print_dog(struct dog *d);
 dog_t *new_dog(char *name, float age, char *owner);
 void free_dog(dog_t *d);
+
 #endif
