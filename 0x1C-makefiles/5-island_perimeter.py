f06a3c733d3a27400d05e6ceb311d53dfb516a06
HANANEZOUBI <hananezoubi1@gmail.com>
2024-02-21 12:47:08 -0800

    h

--- 0x1C-makefiles/5-island_perimeter.py
+++ 0x1C-makefiles/5-island_perimeter.py
@@ -18,11 +18,14 @@
 
     return num
 
+
 def island_perimeter(grid):
     """Returns the perimeter of the island in grid."""
+
     perimeter = 0
     for i in range(len(grid)):
         for j in range(len(grid[i])):
             if grid[i][j]:
                 perimeter += num_water_neighbors(grid, i, j)
+
     return perimeter
